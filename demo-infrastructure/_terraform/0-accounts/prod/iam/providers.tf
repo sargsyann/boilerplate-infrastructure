0a0d69df9d9b653fe91bb02aa3c00130ef3b2ebc
Vazgen Sargsyan <vazgen@dasmeta.com>
2025-05-10 23:20:19 +0400

    add IAM and master secret setup for prod account

---
+++ demo-infrastructure/_terraform/0-accounts/prod/iam/providers.tf
@@ -0,0 +1,12 @@
+## This file and its content are generated based on config, pleas check README.md for more details
+
+provider "aws" {
+
+
+
+
+  default_tags {
+
+    tags = {"Account":"Dasmeta-Demo","AppliedFrom":"terraform-cloud","ManageLevel":"account","ManagedBy":"terraform","TerraformCloudWorkspace":"0-accounts_prod_iam","TerraformModuleSource":"terraform-aws-modules/iam/aws//modules/iam-user","TerraformModuleVersion":"5.30.0"}
+  }
+}
