f3b51699fb4a3de31ffe37e4e5c2143ed5377548
Vazgen Sargsyan <vazgen@dasmeta.com>
2025-05-11 15:56:31 +0400

    add ingress setup for prod

---
+++ demo-infrastructure/_terraform/1-environments/prod/ingress/main.tf
@@ -0,0 +1,25 @@
+## This file and its content are generated based on config, pleas check README.md for more details
+
+module "this" {
+  source  = "dasmeta/modules/aws//modules/ingress"
+  version = "2.14.7"
+
+  additional_hostnames = ["*.demo.dasmeta.com"]
+  alarms = {"enabled":false,"sns_topic":"Default"}
+  certificate_arn = "${data.tfe_outputs.this["1-environments/prod/certificate"].values.results.arn}"
+  healthcheck_path = "/"
+  healthcheck_success_codes = "200-399"
+  hostname = "demo.dasmeta.com"
+  name = "prod-ingress"
+  namespace = "frontend"
+  tls_hosts = ["*.demo.dasmeta.com"]
+  providers = {"aws":"aws","kubernetes":"kubernetes"}
+}
+
+
+data "tfe_outputs" "this" {
+  for_each = { for workspace in ["1-environments/prod/certificate","1-environments/prod/eks"] : workspace => workspace }
+
+  organization = "Demo-Infrastructure"
+  workspace    = replace(each.value, "/[^a-zA-Z0-9_-]+/", "_")
+}
