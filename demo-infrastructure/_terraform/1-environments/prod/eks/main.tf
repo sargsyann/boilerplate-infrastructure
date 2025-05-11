c6f0db827a2eb74b37b785ddd1013335ef1153ed
Vazgen Sargsyan <vazgen@dasmeta.com>
2025-05-11 12:57:45 +0400

    add certificate. EKS and RDS setup for prod

---
+++ demo-infrastructure/_terraform/1-environments/prod/eks/main.tf
@@ -0,0 +1,29 @@
+## This file and its content are generated based on config, pleas check README.md for more details
+
+module "this" {
+  source  = "dasmeta/eks/aws"
+  version = "2.21.2"
+
+  account_id = "091947912116"
+  adot_config = {"accept_namespace_regex":"(prod)","additional_metrics":[],"log_group_name":"adot-logs"}
+  alarms = {"sns_topic":"Default"}
+  cluster_enabled_log_types = ["audit"]
+  cluster_name = "eks-prod"
+  enable_kube_state_metrics = true
+  karpenter = {"configs":{"replicas":1},"enabled":true,"resource_configs":{"nodePools":{"general":{"weight":1}}},"resource_configs_defaults":{"limits":{"cpu":100}}}
+  map_roles = [{"groups":["system:masters"],"rolearn":"arn:aws:iam::091947912116:role/aws-reserved/sso.amazonaws.com/eu-central-1/AWSReservedSSO_AdministratorAccess_ab82266ca76b72c5","username":"AWSReservedSSO_AdministratorAccess_ab82266ca76b72c5"}]
+  node_groups = {"prod_nodes_on_demond":{"capacity_type":"ON_DEMAND","desired_size":1,"max_capacity":1,"max_size":1,"min_size":1}}
+  node_groups_default = {"capacity_type":"SPOT","disk_size":50,"instance_types":["t3.large","t3.medium","t3a.large","m5.large"]}
+  prometheus_metrics = ["kube_node_status_condition","kube_node_status_capacity","kube_deployment_spec_replicas","kube_deployment_status_replicas_available"]
+  users = [{"username":"terraform"}]
+  vpc = {"link":{"id":"${data.tfe_outputs.this["1-environments/prod/vpc"].values.results.id}","private_subnet_ids":"${data.tfe_outputs.this["1-environments/prod/vpc"].values.results.private_subnets}"}}
+  providers = {"aws":"aws"}
+}
+
+
+data "tfe_outputs" "this" {
+  for_each = { for workspace in ["1-environments/prod/vpc"] : workspace => workspace }
+
+  organization = "Demo-Infrastructure"
+  workspace    = replace(each.value, "/[^a-zA-Z0-9_-]+/", "_")
+}
---
+++ demo-infrastructure/_terraform/1-environments/prod/rds-postgres/main.tf
@@ -0,0 +1,37 @@
+## This file and its content are generated based on config, pleas check README.md for more details
+
+module "this" {
+  source  = "dasmeta/rds/aws"
+  version = "1.4.4"
+
+  alarms = {"enabled":true,"sns_topic":"account-alarms-handling"}
+  allocated_storage = 20
+  apply_immediately = true
+  create_cloudwatch_log_group = false
+  create_security_group = true
+  db_name = "proddb"
+  db_password = "${data.tfe_outputs.this["0-accounts/prod/master-secret"].values.results.secrets.POSTGRESS_PASS}"
+  db_username = "postgres"
+  egress_with_cidr_blocks = [{"cidr_blocks":"0.0.0.0/0","from_port":0,"protocol":"-1","to_port":0}]
+  engine = "postgres"
+  engine_version = "15.10"
+  identifier = "rds-proddb"
+  instance_class = "db.t3.medium"
+  multi_az = false
+  port = "5432"
+  skip_final_snapshot = true
+  slow_queries = {"enabled":false,"query_duration":1}
+  storage_encrypted = true
+  storage_type = "gp2"
+  subnet_ids = "${data.tfe_outputs.this["1-environments/prod/vpc"].values.results.public_subnets}"
+  vpc_id = "${data.tfe_outputs.this["1-environments/prod/vpc"].values.results.id}"
+  providers = {"aws":"aws"}
+}
+
+
+data "tfe_outputs" "this" {
+  for_each = { for workspace in ["0-accounts/prod/master-secret","1-environments/prod/vpc"] : workspace => workspace }
+
+  organization = "Demo-Infrastructure"
+  workspace    = replace(each.value, "/[^a-zA-Z0-9_-]+/", "_")
+}
