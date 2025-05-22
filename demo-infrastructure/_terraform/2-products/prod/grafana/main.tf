d28d1447ddcd27e653d65c66ff8301d0df22c5b8
Vazgen Sargsyan <vazgen@dasmeta.com>
2025-05-22 10:20:04 +0400

    add Grafana setup for prod

--- demo-infrastructure/_terraform/2-products/prod/grafana/main.tf
+++ demo-infrastructure/_terraform/2-products/prod/grafana/main.tf
@@ -2,10 +2,10 @@
 
 module "this" {
   source  = "dasmeta/grafana/onpremise"
-  version = "1.16.5"
+  version = "1.17.0"
 
   alerts = {"rules":[{"datasource":"prometheus","equation":"gt","expr":"sum(rate(container_cpu_usage_seconds_total{namespace=\\\"demo\\\",pod=~\\\"frontend.*\\\"}[5m]))","function":"Last","name":"Frontend High CPU Alert","threshold":95}]}
-  application_dashboard = {"data_source":{"type":"prometheus","uid":"prometheus"},"rows":[{"name":"backend-strapi","type":"block/service"},{"name":"frontend","type":"block/service"}],"variables":[{"name":"namespace","options":[{"selected":true,"value":"demo"},{"value":"demo"}]}]}
+  application_dashboard = {"data_source":{"type":"prometheus","uid":"prometheus"},"rows":[{"name":"SLA","type":"block/sla"},{"load_balancer_arn":"arn:aws:elasticloadbalancing:eu-central-1:091947912116:loadbalancer/app/prod-ingress/fe7376c7ea9e4e2c","name":"ALB","region":"eu-central-1","type":"block/alb_ingress"},{"name":"Cloudwatch Metrics","region":"eu-central-1","type":"block/cloudwatch"},{"name":"backend-strapi","type":"block/service"},{"name":"frontend","type":"block/service"}],"variables":[{"name":"namespace","options":[{"selected":true,"value":"demo"},{"value":"demo"}]}]}
   aws_region = "eu-central-1"
   cluster_name = "eks-prod"
   grafana_admin_password = "password"
